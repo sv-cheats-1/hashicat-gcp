222b05992fe69be708d50a812ebba36c6825151b
Martin <martin.viktorov@devoteam.com>
2022-06-14 17:51:42 +0000

    Added the first label

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "hashicat-gcp-pls"
     workspaces {
       name = "hashicat-gcp"
