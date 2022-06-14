54d65ae7c58cef23462878e2125b0f0762db1d8c
Martin <martin.viktorov@devoteam.com>
2022-06-14 18:13:10 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,6 @@
-# module "network" {
-#   source  = "app.terraform.io/hashicat-gcp-pls/network/google"
-#   version = "3.4.0"
-#   # insert required variables here
-# }
-module "vpc" {
-  source  = "terraform-google-modules/network/google"
-  version = "~> 3.0"
+module "network" {
+  source  = "app.terraform.io/hashicat-gcp-pls/network/google"
+  version = "3.4.0"
 
   project_id   = var.project
   network_name = "gaurav-network"
